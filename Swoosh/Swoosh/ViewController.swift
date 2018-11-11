1b102c56a2359dc0042cadc7c5f3391a7042ac98
fredlc <fredlefevre@outlook.com>
2018-11-11 18:21:13 -0500

    Experimenting with frames for adjusting layout

--- Swoosh/Swoosh/ViewController.swift
+++ Swoosh/Swoosh/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bigImg: UIImageView!
+    @IBOutlet weak var swooshLogo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        swooshLogo.frame = CGRect(x: view.frame.size.width / 2 - swooshLogo.frame.size.width / 2  , y: 50, width: swooshLogo.frame.size.width, height: swooshLogo.frame.size.height)
+        
+        bigImg.frame = view.frame;
     }
 
 
