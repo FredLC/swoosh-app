411e39eb8acebcab7a0e3e82294217f472524497
fredlc <fredlefevre@outlook.com>
2018-11-12 13:00:54 -0500

    Added player model and logic on LeagueVC buttons

---
+++ Swoosh/Swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Fred Lefevre on 2018-11-12.
+//  Copyright © 2018 Fred Lefevre. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
