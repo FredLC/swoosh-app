60f089d698180539a63508aecbf543c663f6442e
fredlc <fredlefevre@outlook.com>
2018-11-12 12:40:08 -0500

    Implemented segues and refactored file organization

---
+++ Swoosh/Swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,23 @@
+//
+//  LeagueVC.swift
+//  Swoosh
+//
+//  Created by Fred Lefevre on 2018-11-12.
+//  Copyright © 2018 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
+
+}
--- Swoosh/Swoosh/Controller/WelcomeVC.swift
+++ Swoosh/Swoosh/Controller/WelcomeVC.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
 
 }
 
